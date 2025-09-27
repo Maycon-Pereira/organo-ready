efa1766f2c71c4fe797f0602f775e52402b76892
Maycon-Pereira <pmaycon63@gmail.com>
2025-09-27 13:18:39 -0300

    Add: Finished project upload

---
+++ src/componentes/Rodape/index.js
@@ -0,0 +1,11 @@
+import './Rodape.css'
+
+const Rodape = () => {
+    return (
+        <section className='rodape'>
+            <img src='/img/banner.png' alt='Rodapé do site'/>
+        </section>
+    )
+}
+
+export default Rodape
